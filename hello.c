90706d1b7d5681ca2cd9b77297b19b82fa8bcee2
Riley Wood <rjw245@gmail.com>
2016-06-08 17:56:19 -0400

    Working interrupt on Timer A. This will become the clock tick interrupt for the scheduler

--- hello.c
+++ hello.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
-#include <msp430.h> 
+#include <msp430.h>
 
 /*
  * hello.c
  */
+
+static volatile int printNow=0;
+
+static void setupTimerA();
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+
+    setupTimerA();
+    P1DIR |= 0x01;
 	
-	printf("Hello World!\n");
+	while(1) {
+		if(printNow) {
+			P1OUT ^= 0x01;
+			printNow = 0;
+		}
+	}
 	
 	return 0;
 }
+
+static void setupTimerA() {
+	TA0CTL |= MC_0; //Stop timer A during setup
+	TA0CCR0 = 10000;
+
+	/*
+	 * Clock source: SMCLK
+	 * Mode: Count up to CCR repeatedly (and start)
+	 * Divisor: 1
+	 * and clear the clock
+	 */
+	TA0CTL =  TASSEL__SMCLK | MC__UP | ID__8 | TACLR;
+	TA0CCTL0 = CCIE;
+	__enable_interrupt();
+}
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0_ISR (void)
+{
+  printNow=1;
+}
+

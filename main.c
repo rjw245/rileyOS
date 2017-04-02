30775bf397bdbfb36fca63ff31ee645e8ec54521
Riley Wood <rjw245@gmail.com>
2017-04-01 22:43:31 -0400

    Added a third task which controls the onboard LCD
    For a more advanced demonstration. And it worked great!!!

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <msp430.h>
 #include "rileyOS_config.h"
 #include "scheduler.h"
+#include "hal_LCD.h"
 
-void Task1( void );
-void Task2( void );
+static void Task1( void );
+static void Task2( void );
+static void LCDTask( void );
 
 #define TASK1_STACK_BOTTOM		0x200
 #define TASK2_STACK_BOTTOM		0x400
@@ -17,17 +19,15 @@ int main(void) {
     P4DIR |= 0x01;
 
     scheduler_init();
-
     SCHEDULER_ADD(Task1, 512);
-
     SCHEDULER_ADD(Task2, 512);
-
+    SCHEDULER_ADD(LCDTask, 512);
     scheduler_run();
 
     return 0;
 }
 
-void Task1( void ) {
+static void Task1( void ) {
 	while(1) {
 		//Flash LED P1.0
 		P1OUT ^= 0x01;
@@ -36,11 +36,18 @@ void Task1( void ) {
 	}
 }
 
-void Task2( void ) {
+static void Task2( void ) {
 	while(1) {
 		//Flash LED P4.0
 		P4OUT ^= 0x01;
         static int i;
         for(i=0; i<9000; i++);
 	}
+}
+
+static void LCDTask( void ) {
+    Init_LCD();
+    while(1) {
+        displayScrollText("DEADBEEF0123");
+    }
 }

b11f8d53831035d061944ca73d78deab8db6cb0f
Riley Wood <rjw245@gmail.com>
2017-04-10 21:51:31 -0400

    Declare API in different order, compact comments

--- scheduler.h
+++ scheduler.h
@@ -20,15 +20,8 @@ typedef volatile struct task_private_s {
 } task_t;
 
 
-/**
- * Handy macro which blackboxes the allocation of memory
- * per task. Accepts the task function to schedule
- * and the size of stack to allocate as arguments.
- */
-#define SCHEDULER_ADD(func, stack_size) \
-    CREATE_TASK_HANDLE(__LINE__, func); \
-    CREATE_TASK_STACK(__LINE__, func, stack_size); \
-    CALL_SCHEDULER_ADD(__LINE__, func);
+/// Sets up the idle task
+void scheduler_init( void );
 
 /**
  * Add task to task list to be run at next context switch.
@@ -39,14 +32,17 @@ typedef volatile struct task_private_s {
 void scheduler_add_task(task_t * task_handle, const char * name,
                         task_func_t func, uint16_t * task_stack, uint16_t stack_bytes);
 
-/**
- *  Kicks off the timer interrupt
- */
+/// Kicks off the timer interrupt
 void scheduler_run( void );
 
 /**
- *  Sets up the idle task
+ * Handy macro which blackboxes the allocation of memory
+ * per task. Accepts the task function to schedule
+ * and the size of stack to allocate as arguments.
  */
-void scheduler_init( void );
+#define SCHEDULER_ADD(func, stack_size) \
+    CREATE_TASK_HANDLE(__LINE__, func); \
+    CREATE_TASK_STACK(__LINE__, func, stack_size); \
+    CALL_SCHEDULER_ADD(__LINE__, func);
 
 #endif /* SCHEDULER_H */
